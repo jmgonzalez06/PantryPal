f39762f1b2a70d4f77abaded22f1bc3e2ac7c854
Jose Gonzalez <jmgonzalez06@csu.fullerton.edu>
2025-12-10 16:13:55 -0800

    Initialize Android project with Compose and navigation dependencies
    Initialize Android project with Compose and navigation dependencies

---
+++ app/src/main/java/com/jose/pantrypal/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.jose.pantrypal
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.jose.pantrypal.ui.theme.PantryPalTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PantryPalTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    // TODO: NavHost will go here
+                    Text(
+                        text = "PantryPal Root",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PantryPalTheme {
+        Greeting("Android")
+    }
+}
